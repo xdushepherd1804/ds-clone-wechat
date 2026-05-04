5d5fe9fe87b3cfad894e9cead864826ec74be270
xdushepherd91 <xdushepherd91@gmail.com>
2026-05-04 13:44:23 +0800

    refactor mobile tsconfig as standalone config with path aliases

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -4,8 +4,10 @@ const path = require('path');
 const config = getDefaultConfig(__dirname);
 
 // Resolve @wechat-clone/shared to local shared-src (included in EAS archive)
+// Also resolve @/ aliases to ./src/ for EAS build environment
 config.resolver.extraNodeModules = {
   '@wechat-clone/shared': path.resolve(__dirname, 'shared-src'),
+  '@': path.resolve(__dirname, 'src'),
 };
 
 module.exports = config;
